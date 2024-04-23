a9a519568c63c5098313626045b80f5f45bfdb8c
DaveJuanes <dmjuanes_@hotmail.es>
2024-04-22 21:04:32 -0400

    [CLASS 36] Ciclo de vida de componentes: ngOnInit

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -14,6 +14,7 @@ export class CounterComponent {
   constructor() {
     // NO ASYNC
     // Before render
+    // One time only run
     console.log('constructor');
     console.log('-'.repeat(10));    
   }
@@ -23,7 +24,28 @@ export class CounterComponent {
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
     console.log(changes);
-    
+  }
+
+  ngOnInit() {
+    // after render
+    // una vez
+    // async, then, subs
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));    
+    console.log('duration => ', this.duration);
+    console.log('message => ', this.message);
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // componentes child was rendered or painting
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
   }
 }
 // constructor
